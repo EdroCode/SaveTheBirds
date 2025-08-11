bb11fe1207fb740bf092559d13b26897f034add0
Edro <94792540+EdroCode@users.noreply.github.com>
2025-08-11 20:55:33 +0100

    Update

---
+++ Scenes/Levels/cidade.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var anim = $AnimationPlayer
+
+
+func _process(delta: float) -> void:
+	
+	
+	
+	$Mist.offset.x += 0.09
+	
+	if Input.is_action_just_pressed("Reset"):
+		reload_scene()
+
+
+func reload_scene():
+	get_tree().reload_current_scene()
+
+
+func _on_player_died() -> void:
+	$AnimationPlayer.play("End")
